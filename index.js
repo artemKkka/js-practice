e7d3c288df2aaa6884190965f66f827a44137b83
artemKkka <93820322+artemKkka@users.noreply.github.com>
2023-04-01 14:01:56 +0300

    array methods find, filter, map

--- index.js
+++ index.js
@@ -46,38 +46,87 @@
 
 // for(let i=1; i<=5; i++) {
 //     console.log(i)
+// }                        
+
+ 
+
+// let str = 'Hello World'
+// let newStr = str.toLowerCase()
+// console.log(newStr)
+
+// let count = str.length
+// console.log(count)
+
+// let colors = ["Blue", "Yellow", "Red"];
+// let countarr = colors.length
+// console.log(countarr)
+
+
+
+// let arr = ['kiwi', 'orange', 'apple']
+// arr.splice(0, 2, "banana");
+
+// console.log(arr)
+
+// let newarr = arr.slice(0, 1)
+
+// console.log(newarr)
+
+// for(let fruit of arr) {
+//     console.log(fruit,"!")
 // }
 
 
 
-let str = 'Hello World'
-let newStr = str.toLowerCase()
-console.log(newStr)
-
-let count = str.length
-console.log(count)
-
-let colors = ["Blue", "Yellow", "Red"];
-let countarr = colors.length
-console.log(countarr)
+// let text = 'Hello World !'
+// let newtext = text.split(' ', 1)
+// console.log(newtext)
 
 
 
-let arr = ['kiwi', 'orange', 'apple']
-arr.splice(0, 2, "banana");
+// let obj = { a: 20, b: 10, colors: ["red", "yellow", "blue"] };
+// let sum = obj["b"] + obj["a"]
+// console.log(sum)
 
-console.log(arr)
+// let color = obj["colors"][2]
+// console.log(color)
 
-let newarr = arr.slice(0, 1)
-
-console.log(newarr)
-
-for(let fruit of arr) {
-    console.log(fruit,"!")
-}
-
+let colors = ["Blue", "Yellow", "Red", "White", "Pink"];
+colors.splice(3, 2,"green", "black");
+let colorsToString = colors.join(', ').length
+console.log(colorsToString)
 
 
-let text = 'Hello World !'
-let newtext = text.split(' ', 1)
-console.log(newtext)
+let obj = { colors: ["red", "yellow", "blue"], b: 20, c: 10 };
+let sum = obj['b']+ obj['c']
+console.log(sum)
+
+
+let arrOfObj = [{name: 'Jonh', age: '15'}, {name: 'Jane', age: '24'}, {name: 'Mary', age: '21'}]
+let findAge = arrOfObj.find(item=>item.age>20)
+console.log(findAge)
+
+
+let newArr = [0, 1, "hello", { name: "Alex", age: 20 }, false];
+console.log(newArr.indexOf(1))
+
+
+console.log(colors.includes('green'))
+
+
+let arrOfObj1 = [
+    { name: "Alex", age: 19, dog: false },
+    { name: "Maria", age: 30, dog: true },
+    { name: "John", age: 15, dog: false },
+  ];
+let dogFind = arrOfObj1.map((item)=>{
+    if(item.dog==false) {
+        console.log(item)
+    }
+})
+
+let ageFind = arrOfObj1.map((item)=>{
+    if(item.age>20) {
+        console.log(item)
+    }
+})
